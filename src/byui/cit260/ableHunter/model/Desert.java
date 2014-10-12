bbd9b6666bc89629d123f2766a5930af1439608b
presher <pre12014@byui.edu>
2014-10-12 10:58:04 -0500

    Able Hunter Text Game CIT 260

---
+++ src/byui/cit260/ableHunter/model/Desert.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.ableHunter.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Jason
+ */
+public class Desert implements Serializable{
+    
+}
